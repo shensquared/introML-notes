840d80261b288ef11e0cd29b43eeacd8f8d184b1
Shen Shen <shenshen@mit.edu>
2023-12-17 03:34:11 -0500

    change term

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
     url: "https://introml.mit.edu",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/spring24",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.

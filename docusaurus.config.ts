f2fef08b913c90b27a7528aa55fb8a7bc52e9d0c
Shen Shen <shenshen@mit.edu>
2023-12-21 04:55:10 -0500

    invalid mathjax config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,7 +54,6 @@ const config: Config = {
                                     ],
                                     processEscapes: true,
                                     tags: "ams",
-                                    equationNumbers: { autoNumber: "all" },
                                 },
                             },
                         ],
